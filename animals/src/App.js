3e4599d64911562ec53c6c8a1ca2c86a75b1a08a
Joe <pelikata@gmail.com>
2022-12-04 20:14:57 +0100

    kesz

---
+++ animals/src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+  return (
+    <div className="app">
+      <button onClick={handleClick}>Add animal</button>
+      <p>Start to click on your favourite animal!</p>
+      <div className="animal-list">{renderedAnimals}</div>
+    </div>
+  );
+}
+
+export default App;
